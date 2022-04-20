c9a583c86a5bd0a8327ea1469bc958f62259478f
WJRobotics <61797736+WJRobotics@users.noreply.github.com>
2022-04-20 15:34:56 -0400

    added movement

--- create.c
+++ create.c
@@ -1,10 +1,21 @@
 #include <kipr/botball.h>
 #include "wj.h"
+#define ARMPORT 0
+#define ARMUP 1500
+#define ARMDOWN 200
+
 
 int main(){
   // connect to the roomba
   create_connect();
   // do the other stuff
+  moveServo(ARMPORT, ARMDOWN);
+  
+  moveServo(ARMPORT, ARMUP);
+  
+  create_drive(300, 0);
+  msleep(2000);
+  create_stop();
   
   return 1;
   {
