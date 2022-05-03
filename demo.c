e05ae55a82406052dec112a610e16dc8bfe47222
WJRobotics <61797736+WJRobotics@users.noreply.github.com>
2022-05-03 11:39:00 -0400

    Update demo.c

--- demo.c
+++ demo.c
@@ -17,7 +17,8 @@ motor(0, 300);
 msleep(1000);
 ao();*/
 
-moveServo(0, 200, 1000);
-moveServo(0, 1000, 1000);
+moveServo(0, LIFTER_DOWN, 1000);
+moveServo(0, LIFTER_MID, 1000);
+moveServo(0, LIFTER_DOWN, 1000);
 return 1;
 }
